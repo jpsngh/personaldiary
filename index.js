f2924aa2cabb42657e0851dd4d152caca6ba1b6d
JP Singh <39120590+jpsngh@users.noreply.github.com>
2023-03-01 06:27:57 -0500

    index modified

--- index.js
+++ index.js
@@ -5,18 +5,31 @@ import authRoute from "./routes/auth.js"
 import noteRoute from "./routes/notes.js"
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path  from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 
 const app = express();
 connectDB();
 dotenv.config();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '/client/build')));
+
 
 app.get("/",(req,res)=>{
     res.send("hey");
 })
 app.use('/api/auth',authRoute)
 app.use('/api/notes',noteRoute)
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, './client/build/index.html'));
+  })
+
 
 
 
